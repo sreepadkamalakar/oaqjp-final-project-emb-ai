a2608e287c7041c08215d96f4d08ec8546cfa126
sreepadkamalakar <kamalakar.sripad@gmail.com>
2026-03-12 20:56:58 -0400

    chagned formatted output

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,9 +1,27 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=header)
-    return response.text
+    formatted_response = json.loads(response.text)
+    
+    emotions = formatted_response.get(
+            "emotionPredictions", [{}]
+        )[0].get("emotion", {})
+
+    dominant_emotion = max(emotions, key=emotions.get) if emotions else None
+
+    return {
+            "anger": emotions.get("anger"),
+            "disgust": emotions.get("disgust"),
+            "fear": emotions.get("fear"),
+            "joy": emotions.get("joy"),
+            "sadness": emotions.get("sadness"),
+            "dominant_emotion": dominant_emotion
+        }
+
+
 
