c1ea87b2acc303b8bebeb7ce9addac16d262ae1e
sreepadkamalakar <kamalakar.sripad@gmail.com>
2026-03-16 21:55:02 -0400

    added

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,6 +6,7 @@ def emotion_detector(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=header)
+    print(response.status_code)
     formatted_response = json.loads(response.text)
     
     emotions = formatted_response.get(
@@ -14,14 +15,24 @@ def emotion_detector(text_to_analyse):
 
     dominant_emotion = max(emotions, key=emotions.get) if emotions else None
 
-    return {
-            "anger": emotions.get("anger"),
-            "disgust": emotions.get("disgust"),
-            "fear": emotions.get("fear"),
-            "joy": emotions.get("joy"),
-            "sadness": emotions.get("sadness"),
-            "dominant_emotion": dominant_emotion
-        }
+    if response.status_code == 400:
 
+        return {
+                "anger": None,
+                "disgust": None,
+                "fear": None,
+                "joy": None,
+                "sadness": None,
+                "dominant_emotion": None
+            }
+    
+    else:
 
-
+        return {
+                "anger": emotions.get("anger"),
+                "disgust": emotions.get("disgust"),
+                "fear": emotions.get("fear"),
+                "joy": emotions.get("joy"),
+                "sadness": emotions.get("sadness"),
+                "dominant_emotion": dominant_emotion
+            }
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+''' this is server py file which is main '''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,13 +6,20 @@ app = Flask("emotionDetector")
 
 @app.route("/emotionDetector")
 def emotion_detect():
+    ''' method to call the emotion_detection '''
     text_to_analyze  = request.args.get('textToAnalyze')
-    result = emotion_detector(text_to_analyze )
+    result = emotion_detector(text_to_analyze)
     emotion = result['dominant_emotion']
-    return "For the given statement, the system response is {}. The dominant emotion is {}.".format(result,result['dominant_emotion'])
+    if emotion is None:
+        return "Invalid text! Please try again!."
+    return (
+        f"For the given statement, the system response is {result}. "
+        f"The dominant emotion is {result['dominant_emotion']}."
+        )
 
 @app.route("/")
 def render_index_page():
+    ''' this is docstring for the methdo'''
     return render_template('index.html')
 
 if __name__ == '__main__':
